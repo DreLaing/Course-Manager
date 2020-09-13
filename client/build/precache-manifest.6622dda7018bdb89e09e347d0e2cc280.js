1cf3133dee7bc856d44411d68626f3197ecc7801
DreLaing <43832424+DreLaing@users.noreply.github.com>
2020-09-13 13:09:14 -0600

    first deploy

---
+++ client/build/precache-manifest.6622dda7018bdb89e09e347d0e2cc280.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784d3ea670f20688b6cd2824c1931479",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acd3fd000c009584eab1",
+    "url": "/static/css/main.c103628d.chunk.css"
+  },
+  {
+    "revision": "1a1994fd9bbba3463c58",
+    "url": "/static/js/2.23252b3e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.23252b3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acd3fd000c009584eab1",
+    "url": "/static/js/main.6edd1509.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "b0ca53cf52293d88e0f325caf73e3d5a",
+    "url": "/static/media/google-logo.b0ca53cf.png"
+  },
+  {
+    "revision": "dd01e87a700b24d8d2b15c315bf9af62",
+    "url": "/static/media/hubspot-logo.dd01e87a.png"
+  },
+  {
+    "revision": "6160f4daa0a59073dbe0f04624fc87e4",
+    "url": "/static/media/landing-page-bg-.6160f4da.jpg"
+  },
+  {
+    "revision": "4a91412fe56a193f71d9f97bd6a68ed4",
+    "url": "/static/media/study-image.4a91412f.png"
+  }
+]);
