f868e6038e4ba943f171fd21ecdc82406e55e6c4
DreLaing <43832424+DreLaing@users.noreply.github.com>
2020-09-14 10:00:09 -0600

    deploy

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(cors());
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(root));
-    app.get("*", (req, res) => {
+    app.get("/", (req, res) => {
         res.sendFile('index.html', { root });
     })
 }
